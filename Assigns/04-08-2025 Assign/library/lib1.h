c8049b6be7e5da5bc5a630a39d59391557876949
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2025-08-04 12:37:53 +0530

    code

---
+++ Assigns/04-08-2025 Assign/library/lib1.h
@@ -0,0 +1,8 @@
+#ifndef LIB1_H
+#define LIB1_H
+
+namespace Lib1 {
+    int calculate(int a, int b);  // Declaration
+}
+
+#endif
---
+++ Assigns/04-08-2025 Assign/library/lib2.h
@@ -0,0 +1,8 @@
+#ifndef LIB2_H
+#define LIB2_H
+
+namespace Lib2 {
+    int calculate(int a, int b);  // Declaration
+}
+
+#endif
