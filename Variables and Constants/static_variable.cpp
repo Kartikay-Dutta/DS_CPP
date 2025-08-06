490b6080024d1483ef540834b9d5e587251acec2
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2025-08-06 11:38:47 +0530

    code

---
+++ Variables and Constants/static_variable.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+class Demo{
+    public:
+    static int counter; // Declaration inside class
+    Demo(){counter++;}
+};
+int Demo::counter = 0; // Definition outside class
+
+int main(){
+    Demo a,b;
+    cout<<Demo::counter<<endl; // Prints 2
+    return 0;
+}
