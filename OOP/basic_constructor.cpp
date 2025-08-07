3552246ce36eea826ee4cdfafa911238393e8c46
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2025-08-07 12:19:43 +0530

    code\

--- OOP/basic_constructor.cpp
+++ OOP/basic_constructor.cpp
@@ -1,17 +1,37 @@
+// #include<iostream>
+// using namespace std;
+
+// class Point{
+//     private:
+//      int x;
+//      int y;
+
+//     public:
+//      Point(int a, int b):x(a),y(b){
+//         cout<<"Point initialized with x = " << x<<" and y = "<<y<<endl;
+//      }
+// };
+// int main(){
+//     Point p1(3,4);
+//     return 0;
+// }
+
+
 #include<iostream>
 using namespace std;
 
-class Point{
+class Example{
     private:
-     int x;
-     int y;
+     const int value;
+     int&  ref;
 
     public:
-     Point(int a, int b):x(a),y(b){
-        cout<<"Point initialized with x = " << x<<" and y = "<<y<<endl;
+     Example(int v, int& r): value(v),ref(r){
+        cout<<"Const value: " << value<<" | Refernce value: "<<ref<<endl;
      }
 };
 int main(){
-    Point p1(3,4);
+    int num = 10;
+    Example ex(5,num);
     return 0;
 }
