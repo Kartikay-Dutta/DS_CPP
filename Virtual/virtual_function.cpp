037d1ac5c3f79354719d85a8a0c23bdc846bb6fa
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2025-08-12 12:00:41 +0530

    code

--- Virtual/virtual_function.cpp
+++ Virtual/virtual_function.cpp
@@ -1,26 +1,24 @@
-// Declaration
-
-// virtual void display() = 0; // no initialization and pure virtual function
-
-// ex
-#include<iostream>
+#include <iostream>
 using namespace std;
-class A{
-    public:
-        virtual void draw()=0;
 
-        void show(){
-            cout << "idk dawg\n";
-        }
+class A {
+public:
+
+    virtual void draw() { // virtual so it can be overridden
+        cout << "idk dawg\n";
+    }
 };
-class child : public A{
-    public:
-     void draw() override{
-        cout<<"huh?\n";
-     }
+
+class child : public A {
+public:
+
+    void draw() override {
+        cout << "huh?\n";
+    }
 };
-int main(){
+
+int main() {
     child c;
-    c.show();
     c.draw();
+    
 }
